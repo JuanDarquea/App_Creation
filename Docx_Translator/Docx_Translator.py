37cb470b8ed68dd491b6a7d83b1517816c3a245e
Juan Darquea <juan.darquea.a@gmail.com>
2025-11-20 03:21:53 -0500

    Added to project: File reeading, word and letter count, and text type. All with it's dedicated 'print' as test

--- Docx_Translator/Docx_Translator.py
+++ Docx_Translator/Docx_Translator.py
@@ -2,8 +2,12 @@
 import os
 from tkinter import Tk
 from tkinter import filedialog as fd
+from dotenv import load_dotenv # to load environment variables from .env file
 
 from docx import Document
+
+# load environment variable from .env file
+load_dotenv()
 
 # define a variable to select the file to be translated
 def select_docx_file():
@@ -20,7 +24,8 @@ def select_docx_file():
             ("Word Documents", "*.docx"), # shows only .docx files
             ("All FIles", "*.*") # show every type of file
         ],
-        initialdir=os.path.expanduser("C:\\Users\\USUARIO\\OneDrive\\Documentos\\My_Projects\\Projects\\AppTools\\Docx_Translator") # sets the initial directory to user's home folder
+        # use the environment variable to set the initial directory and a spare default value
+        initialdir=os.getenv("docx_translator_dir", os.getenv("app_tools_dir")) 
     )
     
     # destroy the root dialog window
@@ -31,39 +36,67 @@ def select_docx_file():
 
 def file_validation(file_path):
     """Validate if a file path was selected""" 
-    if file_path is None:
-        return False
-    elif not os.path.exists(file_path):
-        print(f"\nError!! The file {file_path} selected does not exist.")
-        return False
-    elif not file_path.lower().endswith(".docx"):
+    if file_path is None: # when no file is selected
+        return 
+    elif not file_path.lower().endswith(".docx"): # validate file extension
         print("\nError!! The file selected must be a '.docx' file.")
-        return False
+        return 
     else:
-        # When file is selected
-        print(f"\nFile selected to translate: {file_path}", 
-                f"\nFile path: {os.path.dirname(file_path)}", 
-                f"\nFile name: {os.path.basename(file_path)}", 
-                f"\nFile size: {os.path.getsize(file_path)} KB", sep="")
-        return True
+        try: # validate file existence
+            # When file is selected
+            print(f"\nFile selected to translate: {file_path}", 
+                    f"\nFile path: {os.path.dirname(file_path)}", 
+                    f"\nFile name: {os.path.basename(file_path)}", 
+                    f"\nFile size: {os.path.getsize(file_path)} KB", sep="")
+            return True
+        except FileExistsError: # file does not exist
+            print(f"\nError!! The file {file_path} selected does not exist.")
+            return 
+        except Exception as e: # other errors
+            print(f"\nError validating the file: {e}")
+        return
+
 def read_document(file_path):
     """Read the .docx file and return it as an object"""    
     selected_document = Document(file_path)
-    doc = []
-    for paragraph in selected_document.paragraphs:
-        doc.append(paragraph.text)
-    print("\nDocument content read successfully.")
-    
+    doc = [] # create empty list to store paragraphs
+
+    # extract all text paragraphs from the document
+    try: 
+        print("\nReading document content...")
+        for paragraph in selected_document.paragraphs:
+#            if paragraph.text.strip() != "": # skip empty paragraphs
+            doc.append(paragraph.text)
+        # print document content read success message
+        print("\nDocument content read successfully.")
+    except Exception as e: # handle errors while reading document
+        print(f"Error reading the document: {e}")
+        return
+
+    # print paragraph count
     total = len(selected_document.paragraphs)
     print(f"Paragraph count: {total}\n")
-#    print("Document content:")
-    print("First paragraph: \n", 
-          doc[0])  # print first paragraph of document
-    print()
-#    n = 0
-#    for paragraph in doc:
-#        n = n + 1
-#        print(f"P{n}: {paragraph}\n")
+
+    # print first paragraph as test
+#    print("First paragraph: \n", 
+#          doc[0])  # print first paragraph of document
+#    print()
+
+    # print all paragraphs with a paragraph index as test
+    for index, paragraph in enumerate(doc):
+        if paragraph.strip() != "": # skip empty paragraphs
+            print(index + 1, 
+                  paragraph, selected_document.paragraphs[index].style.name, 
+                  f"{len(selected_document.paragraphs[index].text)} characters", 
+                  sep = " - ")
+#            print(f"P{index + 1}: {paragraph}") # alternative print format
+        else:
+            print(index +1, 
+                  "<Empty paragraph>", 
+                  selected_document.paragraphs[index].style.name, 
+                  f"{len(selected_document.paragraphs[index].text)} characters", 
+                  sep = " - ")
+#            index - 1 # do not count empty paragraphs
 
 def main():
     """Main function to test file selection"""
@@ -74,7 +107,8 @@ def main():
 
     # Close if cancelled and no file selected
     if not chosen_file:
-        print("\nUser closed window before selecting a file. Goodbye!")
+        print("\nUser closed the window before selecting a file.", 
+              "\nGoodbye!")
         return
 
     # Validate file
